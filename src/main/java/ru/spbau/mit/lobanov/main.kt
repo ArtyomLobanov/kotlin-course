eba2b004514c726e556abdc7cf817d5c73394b8c
ArtyomLobanov <lobanov98@yandex.ru>
2017-10-29 22:31:45 +0300

    Solve problem

---
+++ src/main/java/ru/spbau/mit/lobanov/main.kt
@@ -0,0 +1,183 @@
+package ru.spbau.mit.lobanov
+
+import java.lang.Math.abs
+
+/**
+ * Represents quarters of plane R^2
+ *
+ * @property tgFinite true if y/x if finite for every point (x, y) from this quarter
+ * @constructor Creates an empty group.
+ */
+enum class Quarter(val tgFinite: Boolean) {
+    FIRST(true),
+    SECOND(false),
+    THIRD(true),
+    FOURTH(false);
+}
+
+/**
+ * Represents figures from Q + {+Inf, -Inf}
+ *
+ * @property numerator is numerator of represented figure. May be negative.
+ * @property denominator is denominator of represented figure. May be negative.
+ * @property signum is signum of represented value (-1, 0 or 1)
+ * @constructor Creates Representation of numerator/denominator
+ */
+data class Rational(
+        private val numerator: Long,
+        private val denominator: Long
+) : Comparable<Rational> {
+
+    val signum = when {
+        denominator != 0L -> signum(numerator) * signum(denominator)
+        else -> signum(numerator)
+    }
+
+    override fun compareTo(other: Rational): Int {
+        if (signum != other.signum) {
+            return signum - other.signum
+        }
+        val difference = abs(numerator * other.denominator) - abs(other.numerator * denominator)
+        return signum * signum(difference)
+    }
+
+    override fun equals(other: Any?): Boolean {
+        return if (other === null || other !is Rational) false else compareTo(other) == 0
+    }
+
+    override fun hashCode(): Int {
+        throw UnsupportedOperationException("It's not easy to define hash code for this class")
+    }
+}
+
+/**
+ * Represents figures from Q + {+Inf, -Inf}
+ *
+ * @property x is x cartesian coordinate
+ * @property y is y cartesian coordinate
+ * @property angle is angle between the vector and the X-axis when traversing counter-clockwise
+ * @constructor Creates radius vector to point (x, y)
+ */
+data class Vector2D(private val x: Long, private val y: Long) {
+    val angle = Angle(x, y)
+
+    /**
+     * Calculate cross product of vectors
+     * @return z-coordinate of cross product of vectors
+     */
+    infix fun cross(other: Vector2D): Long {
+        return x * other.y - y * other.x
+    }
+
+    /**
+     * Calculate dot product of vectors
+     * @return dot product
+     */
+    infix fun dot(other: Vector2D): Long {
+        return x * other.x + y * other.y
+    }
+}
+
+/**
+ * Represents angle from [0, 2 * Pi)
+ *
+ * @property tg is tangent of angle
+ * @property ctg is cotangent of angle
+ * @constructor Creates angle between the radius vector to point (x, y) and the X-axis when
+ *      traversing counter-clockwise
+ */
+class Angle constructor(x: Long, y: Long) :
+        Comparable<Angle> {
+    private val tg = Rational(y, x)
+    private val ctg = Rational(x, y)
+    private val quarter = quarterOf(x, y)
+
+    override fun compareTo(other: Angle): Int = when {
+        quarter != other.quarter -> quarter.compareTo(other.quarter)
+        quarter.tgFinite -> tg.compareTo(other.tg)
+        else -> -ctg.compareTo(other.ctg) // cotangent is a decreasing function
+    }
+
+    override fun equals(other: Any?): Boolean {
+        return if (other === null || other !is Angle) false else compareTo(other) == 0
+    }
+
+    override fun hashCode(): Int {
+        throw UnsupportedOperationException("It's not easy to define hash code for this class")
+    }
+
+    companion object {
+        /**
+         * Calculate angle between vectors when traversing counter-clockwise
+         * @return angle between vectors
+         */
+        fun between(v1: Vector2D, v2: Vector2D): Angle {
+            val dotProduct = v1 dot v2 // = |V1|*|V2|*cos(a)
+            val crossProduct = v1 cross v2// = |V1|*|V2|*sin(a)
+            return Angle(dotProduct, crossProduct)
+        }
+    }
+
+    override fun toString(): String {
+        return "Angle(tg=$tg,ctg=$ctg)"
+    }
+}
+
+/**
+ * @return mathematical signum function of value
+ */
+fun signum(value: Long): Int {
+    return when {
+        value > 0 -> 1
+        value < 0 -> -1
+        else -> 0
+    }
+}
+
+/**
+ * @return quarter which contains point (x, y)
+ */
+fun quarterOf(x: Long, y: Long): Quarter {
+    return when {
+        x > 0 && y >= 0 -> Quarter.FIRST
+        x <= 0 && y > 0 -> Quarter.SECOND
+        x < 0 && y <= 0 -> Quarter.THIRD
+        x >= 0 && y < 0 -> Quarter.FOURTH
+        else -> throw IllegalArgumentException("Quarter isn't defined for point (0, 0)")
+    }
+}
+
+/**
+ * Shift list to left: x:tail -> tail:x
+ * @return shifted list
+ */
+fun <E> shiftedList(list: List<E>): List<E> =
+        if (list.size > 1) list.drop(1) + list.first() else list
+
+/**
+ * Finds pair of vectors which have minimal unsigned angle between them
+ * @return pair of indexes of vectors in given list
+ */
+fun solve(vectors: List<Vector2D>): Pair<Int, Int>? {
+    val sortedList = vectors.sortedWith(Comparator { x, y -> x.angle.compareTo(y.angle) })
+    val result = sortedList.zip(shiftedList(sortedList))
+            .minBy { (x, y) -> Angle.between(x, y) }
+    return when (result) {
+        null -> null
+        else -> Pair(vectors.indexOf(result.first) + 1, vectors.indexOf(result.second) + 1)
+    }
+}
+
+fun main(args: Array<String>) {
+    val n = readLine()!!.toInt()
+    val vectors = ArrayList<Vector2D>()
+    for (i in 0 until n) {
+        val (x, y) = readLine()!!.split(' ').map(String::toLong)
+        vectors.add(Vector2D(x, y))
+    }
+    val result = solve(vectors)
+    when (result) {
+        null -> throw IllegalArgumentException("Wrong input: n < 2")
+        else -> print("${result.first} ${result.second}")
+    }
+}
---
+++ src/test/java/ru/spbau/mit/lobanov/test.kt
@@ -0,0 +1,178 @@
+package ru.spbau.mit.lobanov
+
+import org.junit.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+
+class UtilsTest {
+    @Test
+    fun testSignum() {
+        assertEquals(1, signum(566L))
+        assertEquals(-1, signum(-566L))
+        assertEquals(0, signum(0L))
+        assertEquals(0, signum(-0L))
+    }
+
+    @Test
+    fun testShiftList() {
+        assertEquals(listOf<Int>(), shiftedList(listOf<Int>()))
+        assertEquals(listOf(1), shiftedList(listOf(1)))
+        assertEquals(listOf(2, 1), shiftedList(listOf(1, 2)))
+        assertEquals(listOf(2, 3, 1), shiftedList(listOf(1, 2, 3)))
+        assertEquals(listOf(2, 3, 4, 5, 6, 1), shiftedList(listOf(1, 2, 3, 4, 5, 6)))
+    }
+}
+
+class QuartersTests {
+    @Test
+    fun testQuarters() {
+        assertEquals(Quarter.FIRST, quarterOf(1, 1))
+        assertEquals(Quarter.SECOND, quarterOf(-1, 1))
+        assertEquals(Quarter.THIRD, quarterOf(-1, -1))
+        assertEquals(Quarter.FOURTH, quarterOf(1, -1))
+
+
+        assertEquals(Quarter.FIRST, quarterOf(1, 0))
+        assertEquals(Quarter.SECOND, quarterOf(0, 1))
+        assertEquals(Quarter.THIRD, quarterOf(-1, 0))
+        assertEquals(Quarter.FOURTH, quarterOf(0, -1))
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun testZeroPointQuarter() {
+        quarterOf(0, 0)
+    }
+
+    @Test
+    fun testOrder() {
+        assertTrue(Quarter.FIRST < Quarter.SECOND)
+        assertTrue(Quarter.SECOND < Quarter.THIRD)
+        assertTrue(Quarter.THIRD < Quarter.FOURTH)
+    }
+}
+
+class RationalTests {
+    @Test
+    fun testSignum() {
+        assertEquals(1, Rational(1, 2).signum)
+        assertEquals(1, Rational(-1, -2).signum)
+        assertEquals(-1, Rational(1, -2).signum)
+        assertEquals(-1, Rational(-1, 2).signum)
+        assertEquals(0, Rational(0, 2).signum)
+        assertEquals(0, Rational(0, -2).signum)
+        // infinity is signed
+        assertEquals(1, Rational(2, 0).signum)
+        assertEquals(-1, Rational(-2, 0).signum)
+    }
+
+    @Test
+    fun testOrder() {
+        assertTrue(Rational(1, 1) == Rational(2, 2))
+        assertTrue(Rational(-1, -1) == Rational(-2, -2))
+        assertTrue(Rational(-1, 1) == Rational(2, -2))
+        assertTrue(Rational(1, 0) == Rational(2, 0))
+        assertTrue(Rational(-1, 0) == Rational(-2, 0))
+        assertTrue(Rational(1, 0) != Rational(-1, 0))
+        assertTrue(Rational(1, 2) < Rational(1, 1))
+        assertTrue(Rational(1, 2) < Rational(2, 2))
+        assertTrue(Rational(1, 2) > Rational(2, -2))
+        assertTrue(Rational(-1, 0) < Rational(1, 2))
+        assertTrue(Rational(1, 0) > Rational(1, 2))
+        assertTrue(Rational(1, 0) > Rational(-1, 0))
+    }
+}
+
+class Vector2DTests {
+    @Test
+    fun testDotProduct() {
+        assertEquals(4, Vector2D(1, 1) dot Vector2D(2, 2))
+        assertEquals(2, Vector2D(-1, -1) dot Vector2D(-1, -1))
+        assertEquals(-4, Vector2D(-1, -1) dot Vector2D(2, 2))
+        assertEquals(0, Vector2D(-1, -1) dot Vector2D(-1, 1))
+    }
+
+    @Test
+    fun testCrossProduct() {
+        assertEquals(0, Vector2D(1, 1) cross Vector2D(2, 2))
+        assertEquals(4, Vector2D(3, 1) cross Vector2D(2, 2))
+        assertEquals(0, Vector2D(1, 1) cross Vector2D(-2, -2))
+        assertEquals(4, Vector2D(1, 1) cross Vector2D(-2, 2))
+        assertEquals(-4, Vector2D(-2, 2) cross Vector2D(1, 1))
+    }
+}
+
+class AngleTests {
+
+    private fun signum(value: Int) = when {
+        value > 0 -> 1
+        value < 0 -> -1
+        else -> 0
+    }
+
+    @Test
+    fun testOrder() {
+        val correctOrder = listOf(
+                Angle(1, 0),
+                Angle(2, 1),
+                Angle(1, 1),
+                Angle(1, 2),
+                Angle(0, 1),
+                Angle(-1, 2),
+                Angle(-1, 1),
+                Angle(-2, 1),
+                Angle(-1, 0),
+                Angle(-2, -1),
+                Angle(-1, -1),
+                Angle(-1, -2),
+                Angle(0, -1),
+                Angle(1, -2),
+                Angle(1, -1),
+                Angle(2, -1)
+        )
+        for (i in correctOrder.indices) {
+            for (j in correctOrder.indices) {
+                assertEquals(signum(i.compareTo(j)), signum(correctOrder[i].compareTo(correctOrder[j])))
+            }
+        }
+    }
+
+    @Test
+    fun testAngleBetweenVectors() {
+        assertEquals(Angle(0, 1), Angle.between(Vector2D(1, 1), Vector2D(-1, 1)))
+        assertEquals(Angle(-1, 0), Angle.between(Vector2D(1, 0), Vector2D(-1, 0)))
+        assertEquals(Angle(-1, 0), Angle.between(Vector2D(0, 1), Vector2D(0, -1)))
+        assertEquals(Angle(-1, 0), Angle.between(Vector2D(0, -1), Vector2D(0, 1)))
+        assertEquals(Angle(1, 0), Angle.between(Vector2D(0, -1), Vector2D(0, -1)))
+        assertEquals(Angle(1, 0), Angle.between(Vector2D(14, -91), Vector2D(14, -91)))
+    }
+}
+
+
+class SolutionTests {
+    @Test
+    fun test1() {
+        val vectors = listOf(
+                Vector2D(-1, 0),
+                Vector2D(0, -1),
+                Vector2D(1, 0),
+                Vector2D(1, 1)
+        )
+        val correctAnswers = listOf(Pair(3, 4), Pair(4, 3))
+        assertTrue(correctAnswers.contains(solve(vectors)))
+    }
+
+    @Test
+    fun test2() {
+        val vectors = listOf(
+                Vector2D(-1, 0),
+                Vector2D(0, -1),
+                Vector2D(1, 0),
+                Vector2D(1, 1),
+                Vector2D(-4, -5),
+                Vector2D(-4, -6)
+        )
+        val correctAnswers = listOf(Pair(6, 5), Pair(5, 6))
+        assertTrue(correctAnswers.contains(solve(vectors)))
+    }
+}
+
